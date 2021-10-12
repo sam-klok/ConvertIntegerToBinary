ea7df95f57358e3279eb973d484da8f0fff4080b
sam-klok <sklok11@gmail.com>
2021-10-11 21:46:42 -0700

    Initial commit

---
+++ ConvertIntegerToBinary/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace ConvertIntegerToBinary
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 999; i++)
+            {
+                var b = ConvertIntToBin(i);
+                var s = MaxCountOf1(b);
+
+                Console.WriteLine($"{i,9} = {b,9}; {s,3}");
+            }
+
+            //Console.WriteLine(MaxCountOf1("110110111")); // 439 -> 3
+
+
+
+        }
+
+        static string ConvertIntToBin(long i)
+        {
+            string digit = Convert.ToString(i % 2);  // remainder
+
+            if (i >= 2)
+            {
+                long remaining = i / 2;
+                string remainingString = ConvertIntToBin(remaining);
+                return remainingString + digit;
+            }
+            return digit;
+        }
+
+
+        static int MaxCountOf1(string s)
+        {
+            int max = 0;
+            int tempMax = 0;
+
+            foreach (var c in s)
+            {
+                if (c == '1')
+                {
+                    tempMax += 1;
+                    continue;
+                }
+
+                if (tempMax > max)
+                    max = tempMax;
+                
+                tempMax = 0;
+            }
+
+            if (tempMax > max)
+                max = tempMax;
+
+ 
+            return max;
+        }
+
+
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,11 @@
+Task #2
+Convert integer to binary number.
+
+https://en.wikipedia.org/wiki/Binary_number
+https://stackoverflow.com/questions/2954962/convert-integer-to-binary-in-c-sharp/2955001
+https://stackoverflow.com/questions/1838963/easy-and-fast-way-to-convert-an-int-to-binary
+
+Task #2
+Count max quontity of "1" in the binary number.
+(Consecutive 1's in Binary Numbers)
+https://www.hackerrank.com/challenges/linkedin-practice-binary-numbers/problem
